73444c2e56d7d832b084ac8183ac8acd9cea95b6
Shafayat Hossain <shafayat@engineer.com>
2025-05-28 04:41:01 +0600

    Deploy to GitHub Pages

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import FloatingBubblesBackground from "../components/floating-bubbles"
-
-export default function Page() {
-  return (
-    <FloatingBubblesBackground name="Shafayat Hossain" title1="Web Developer" title2="Digital Marketing Consultant" />
-  )
-}
